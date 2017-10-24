49e98495146aaf7e814be1e1a12a9a9d02d8e01f
Zrust, Vladimir <vladimir.zrust@merck.com>
2017-10-24 15:46:48 +0200

    Android app prototype

--- AndroidNative/HelloWorld/app/src/main/java/com/example/zrust/helloworld/MainActivity.java
+++ AndroidNative/HelloWorld/app/src/main/java/com/example/zrust/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.zrust.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    void buttonOnClick(android.view.View view){
+    void buttonOnClick(View view){
         System.out.println("You clicked me! :)");
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
+        EditText text = (EditText) findViewById(R.id.editText);
+        textView3.setText(text.getText());
     }
 }
