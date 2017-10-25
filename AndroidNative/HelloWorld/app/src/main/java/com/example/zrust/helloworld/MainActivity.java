c24d5bc897242836213c855b6f480573491a48cd
Vrsek, David <david.vrsek@merck.com>
2017-10-25 22:21:39 +0200

    On click method made public in Android APP

--- AndroidNative/HelloWorld/app/src/main/java/com/example/zrust/helloworld/MainActivity.java
+++ AndroidNative/HelloWorld/app/src/main/java/com/example/zrust/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.zrust.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    void buttonOnClick(View view){
+    public void buttonOnClick(View view){
         System.out.println("You clicked me! :)");
         TextView textView3 = (TextView) findViewById(R.id.textView3);
         EditText text = (EditText) findViewById(R.id.editText);
